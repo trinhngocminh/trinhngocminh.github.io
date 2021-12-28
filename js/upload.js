1a06bc50e10729defe08c88faa468e6b5ecdf995
trinhngocminh <95848209+trinhngocminh@users.noreply.github.com>
2021-12-28 19:24:20 +0700

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -19,7 +19,7 @@ var feedback = function(res) {
         var get_link = res.data.link.replace(/^http:\/\//i, 'https://');
         document.querySelector('.status').classList.add('bg-success');
         var content =
-            'Image : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' 
+            'Link : ' + '<br><input class="image-url" value=\"' + get_link + '\"/>' 
              + '<img class="img" alt="Imgur-Upload" src=\"' + get_link + '\"/>';
         addImg('.status', content);
     }
